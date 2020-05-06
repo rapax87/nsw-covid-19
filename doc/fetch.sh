38180c83b7b0a28c7f625804da6302f8aeed0e67
Xu Bingbing <xubb@hotmail.com>
2020-05-06 18:11:07 +1000

    5/6

---
+++ doc/fetch.sh
@@ -0,0 +1,3 @@
+rm ./*.csv
+wget https://data.nsw.gov.au/data/dataset/97ea2424-abaf-4f3e-a9f2-b5c883f42b6a/resource/2776dbb8-f807-4fb2-b1ed-184a6fc2c8aa/download/covid-19-cases-by-notification-date-location-and-likely-source-of-infection.csv
+wget https://data.nsw.gov.au/data/dataset/3dc5dc39-40b4-4ee9-8ec6-2d862a916dcf/resource/24b34cb5-8b01-4008-9d93-d14cf5518aec/download/covid-19-cases-by-notification-date-and-age-range.csv
